47bba2fbc578a92d2c9ac9de5d445fbcb0ae63ed
Chung-Yi Chi <cychi1210@gmail.com>
2014-05-24 02:16:19 -0400

    Use test settings in tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -12,9 +12,14 @@ from santa import create_app
 class TestBase(unittest.TestCase):
 
     def setUp(self):
+        # Set environment variable to override settings with test settings.
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        os.environ['EVE_SETTINGS'] = current_dir+'/../config/settings_test.py'
+
         self.app = create_app()
+        self.c = self.app.config # set an alias for configs
         self.test_client = self.app.test_client()
-        self.connection = None
+        self.conn = None
         self.setupDB()
 
     def tearDown(self):
@@ -22,8 +27,8 @@ class TestBase(unittest.TestCase):
         del self.app
 
     def setupDB(self):
-        self.connection = MongoClient(self.app.config['MONGO_HOST'], self.app.config['MONGO_PORT'])
-        self.db = self.connection[self.app.config['MONGO_DBNAME']]
+        self.conn = MongoClient(self.c['MONGO_HOST'], self.c['MONGO_PORT'])
+        self.db = self.conn[self.c['MONGO_DBNAME']]
         self.db.client_apps.insert({
             'client_id'     : 'rudy-test',
             'client_secret' : 'rudy-secret',
@@ -32,9 +37,9 @@ class TestBase(unittest.TestCase):
         self.db.users.insert({'id': 'takoman'})
 
     def dropDB(self):
-        self.connection = MongoClient(self.app.config['MONGO_HOST'], self.app.config['MONGO_PORT'])
-        self.connection.drop_database(self.app.config['MONGO_DBNAME'])
-        self.connection.close()
+        self.conn = MongoClient(self.c['MONGO_HOST'], self.c['MONGO_PORT'])
+        self.conn.drop_database(self.c['MONGO_DBNAME'])
+        self.conn.close()
 
 if __name__ == '__main__':
     unittest.main()
