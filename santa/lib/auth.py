bcbc735a92dee7c9cafb748ded14a72f05fcea6f
Chung-Yi Chi <cychi1210@gmail.com>
2014-05-27 00:57:45 -0400

    Change token auth to check X-XAPP-TOKEN header. Add /api/v1/ prefix for api endpoints.

--- santa/lib/auth.py
+++ santa/lib/auth.py
@@ -1,5 +1,5 @@
 import bcrypt
-from flask import current_app as app
+from flask import request, current_app as app
 from eve.auth import BasicAuth, TokenAuth
 
 class BCryptAuth(BasicAuth):
@@ -14,7 +14,19 @@ class BCryptAuth(BasicAuth):
         return user and bcrypt.hashpw(password, user['password']) == user['password']
 
 class AppTokenAuth(TokenAuth):
+    # Override TokenAuth to use the X-XAPP-TOKEN header
+    def authorized(self, allowed_roles, resource, method):
+        """ Validates the the current request is allowed to pass through.
+
+        :param allowed_roles: allowed roles for the current request, can be a
+                              string or a list of roles.
+        :param resource: resource being requested.
+        """
+        auth = request.headers.get('X-XAPP-TOKEN')
+        return auth and self.check_auth(auth, allowed_roles, resource, method)
+
     def check_auth(self, token, allowed_roles, resource, method):
+        print "in check_auth"
         # use Eve's own db driver; no additional connections/resources are used
         client_apps = app.data.driver.db['client_apps']
         lookup = {'token': token}
