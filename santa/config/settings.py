315000b2c6590a17ccf84fe74fb01fe21defadea
Chung-Yi Chi <cychi1210@gmail.com>
2014-06-22 23:51:09 -0400

    Move config into santa app.

---
+++ santa/config/settings.py
@@ -0,0 +1,20 @@
+DEBUG = True
+
+MONGODB_HOST      = 'localhost'
+MONGODB_PORT      = 27017
+# MONGODB_USERNAME  = ''
+# MONGODB_PASSWORD  = ''
+MONGODB_DBNAME    = 'santa_local'
+
+#
+# Customized configuration
+#
+
+# Be sure to restart your server when you modify this file.
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TOKEN_TRUST_KEY = '33578d92d919ca4aaaec930ee4d3d5df754f057e61e93a2247ab7fe13d712ff4072aa6c327601b7009d5bf998f9c70795c1f81ae25f48cfc4607f6c2cc765b48'
+
+MANDRILL_API_KEY = 'THdzAyk7NFSJmH4Qn04cOQ'
