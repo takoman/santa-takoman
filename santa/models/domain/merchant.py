466137381905e42c46607ebc3b0370de0fee5d98
Chung-Yi Chi <cychi1210@gmail.com>
2015-01-24 01:08:48 -0500

    Add basic Merchant model and tests.

---
+++ santa/models/domain/merchant.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from mongoengine import *
+from santa.models.mixins.updated_at_mixin import UpdatedAtMixin
+from santa.models.domain import *
+import datetime, pycountry
+
+__all__ = ('Merchant',)
+
+SUPPORTED_COUNTRIES = [c.alpha2.encode('utf-8') for c in pycountry.countries]
+
+#
+# merchant (noun)
+#   :someone who buys and sells goods especially in large amounts; dealer; trader.
+#
+
+class Merchant(UpdatedAtMixin, Document):
+    user              = ReferenceField(User, required=True)
+    source_countries  = ListField(StringField(choices=SUPPORTED_COUNTRIES))
+    # allpay_account  = ReferenceField(PaymentAccount)
+    updated_at        = DateTimeField(default=datetime.datetime.now)
+    created_at        = DateTimeField(default=datetime.datetime.now)
+
+    meta = {
+        'collection': 'merchants'
+    }
+
+    def send_welcome_email(self):
+        pass
