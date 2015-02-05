cdbbe1965d1c5f01ce7614935456d5492b2ba8c4
Chung-Yi Chi <cychi1210@gmail.com>
2015-02-04 22:07:58 -0500

    Add GET/POST/PUT/DELETE a order line item endpoints and specs.

--- santa/apps/api/v1/order_line_items.py
+++ santa/apps/api/v1/order_line_items.py
@@ -16,7 +16,7 @@ order_line_items = Blueprint('order_line_items', __name__)
 def get_order_line_items():
     order_id = request.args.get('order_id', None)
     if not order_id:
-        raise ApiException('missing order_id param', 404)
+        raise ApiException('missing order id', 404)
 
     order = Order.objects(id=order_id).first()
     if not order:
@@ -26,3 +26,69 @@ def get_order_line_items():
         OrderLineItem.objects(order=order), request.args), request.args)
 
     return render_json(me_to_json(paginated_and_sorted))
+
+@order_line_items.route('/order_line_items/<order_line_item_id>', methods=['GET'])
+@require_app_auth
+def get_order_line_item(order_line_item_id):
+    item = OrderLineItem.objects(id=order_line_item_id).first()
+
+    if not item:
+        raise ApiException('order line item not found', 404)
+
+    return render_json(me_to_json(item))
+
+@order_line_items.route('/order_line_items', methods=['POST'])
+@require_app_auth
+def create_order_line_item():
+    data = parse_request(request)
+
+    order_id = data.get('order')
+    product_id = data.get('product')
+
+    if not order_id:
+        raise ApiException('missing order id', 404)
+
+    if not Order.objects(id=order_id).first():
+        raise ApiException('order not found', 404)
+
+    if product_id and not Product.objects(id=product_id).first():
+        raise ApiException('product not found', 404)
+
+    new_item = OrderLineItem(**{ k: v for (k, v) in data.iteritems() if k in OrderLineItem._fields.keys() })
+
+    new_item.save()
+
+    return render_json(me_to_json(new_item), status=201)
+
+@order_line_items.route('/order_line_items/<order_line_item_id>', methods=['PUT'])
+@require_app_auth
+def update_order_line_item(order_line_item_id):
+    data = parse_request(request)
+
+    known_fields = OrderLineItem._fields.keys()
+    order_line_item = OrderLineItem.objects(id=order_line_item_id).first()
+    if not order_line_item:
+        raise ApiException('order line item not found', 404)
+
+    for k, v in data.iteritems():
+        if k in known_fields:
+            if (isinstance(OrderLineItem._fields[k], ReferenceField)):
+                v = OrderLineItem._fields[k].document_type.objects(id=v).first()
+                if not v:
+                    raise ApiException(k + ' not found')
+            setattr(order_line_item, k, v)
+    order_line_item.save()
+
+    return render_json(me_to_json(order_line_item))
+
+@order_line_items.route('/order_line_items/<order_line_item_id>', methods=['DELETE'])
+@require_app_auth
+def delete_order_line_item(order_line_item_id):
+    order_line_item = OrderLineItem.objects(id=order_line_item_id).first()
+
+    if not order_line_item:
+        raise ApiException('order line item not found', 404)
+
+    order_line_item.delete()
+
+    return render_json(me_to_json(order_line_item))
--- tests/apps/api/v1/order_line_items_tests.py
+++ tests/apps/api/v1/order_line_items_tests.py
@@ -58,7 +58,7 @@ class OrderLineItemsEndpointsTests(AppTestCase):
         res = self.test_client.get('/api/v1/order_line_items',
                                    headers={'X-XAPP-TOKEN': 'rudy-token'})
         self.assertEqual(res.status_code, 404)
-        self.assertIn("missing order_id param", res.data)
+        self.assertIn("missing order id", res.data)
 
     def test_rudy_access_order_line_items_with_non_existing_order(self):
         res = self.test_client.get('/api/v1/order_line_items?order_id=' + str(ObjectId()),
@@ -79,40 +79,132 @@ class OrderLineItemsEndpointsTests(AppTestCase):
     # GET /order_line_items/<order_line_item_id>
     #
     def test_get_certain_order_line_item(self):
-        pass
+        for i, item in enumerate(self.items):
+            res = self.test_client.get(
+                '/api/v1/order_line_items/' + str(item.id), headers={'X-XAPP-TOKEN': 'rudy-token'})
+            self.assertEqual(res.status_code, 200)
+            fetched_item = json.loads(res.get_data())
+            self.assertDictContainsSubset(self.items_dict[i], fetched_item)
 
     def test_get_certain_order_line_item_with_non_existing_id(self):
-        pass
+        res = self.test_client.get(
+            '/api/v1/order_line_items/' + str(ObjectId()), headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("order line item not found", res.data)
 
     #
     # POST /order_line_items
     #
     def test_add_line_item_without_an_order(self):
-        pass
+        product = Product(title=u'Coach Darcy 蝴蝶結長夾(黑色)').save()
+        item_dict = {
+            'type': 'product',
+            'price': 399.50,
+            'quantity': 1,
+            'product': str(product.id)
+        }
+        res = self.test_client.post('/api/v1/order_line_items',
+                                    data=json.dumps(item_dict),
+                                    content_type='application/json',
+                                    headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("missing order id", res.data)
 
     def test_add_line_item_to_a_non_existing_order(self):
-        pass
+        product = Product(title=u'Coach Darcy 蝴蝶結長夾(黑色)').save()
+        item_dict = {
+            'type': 'product',
+            'price': 399.50,
+            'quantity': 1,
+            'order': str(ObjectId()),
+            'product': str(product.id)
+        }
+        res = self.test_client.post('/api/v1/order_line_items',
+                                    data=json.dumps(item_dict),
+                                    content_type='application/json',
+                                    headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("order not found", res.data)
+
+    def test_add_line_item_with_a_non_existing_product(self):
+        item_dict = {
+            'type': 'product',
+            'price': 399.50,
+            'quantity': 1,
+            'order': str(self.order.id),
+            'product': str(ObjectId())
+        }
+        res = self.test_client.post('/api/v1/order_line_items',
+                                    data=json.dumps(item_dict),
+                                    content_type='application/json',
+                                    headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("product not found", res.data)
 
     def test_add_line_item_to_an_order(self):
-        pass
+        product = Product(title=u'Coach Darcy 蝴蝶結長夾(黑色)').save()
+        item_dict = {
+            'type': 'product',
+            'price': 399.50,
+            'quantity': 1,
+            'order': str(self.order.id),
+            'product': str(product.id)
+        }
+        res = self.test_client.post('/api/v1/order_line_items',
+                                    data=json.dumps(item_dict),
+                                    content_type='application/json',
+                                    headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 201)
+        created_item = json.loads(res.get_data())
+        self.assertDictContainsSubset(item_dict, created_item)
 
     #
     # PUT /order_line_items/<order_line_item_id>
     #
     def test_update_a_non_existing_order_line_item(self):
-        pass
+        res = self.test_client.put('/api/v1/order_line_items/' + str(ObjectId()),
+                                   data=json.dumps({}),
+                                   content_type='application/json',
+                                   headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("order line item not found", res.data)
 
     def test_update_an_order_line_item(self):
-        pass
+        item = self.items[0]
+        product = Product(title=u'2015 Coach Darcy 蝴蝶結長夾(黑色)').save()
+        updated_item_dict = {
+            'type': 'commission',
+            'price': 50.50,
+            'quantity': 2,
+            'product': str(product.id)
+        }
+        res = self.test_client.put('/api/v1/order_line_items/' + str(item.id),
+                                   data=json.dumps(updated_item_dict),
+                                   content_type='application/json',
+                                   headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 200)
+        updated_item = json.loads(res.get_data())
+        self.assertDictContainsSubset(updated_item_dict, updated_item)
 
     #
     # DELETE /order_line_items/<order_line_item_id>
     #
     def test_delete_an_order_line_item(self):
-        pass
+        res = self.test_client.get('api/v1/order_line_items/' + str(self.items[0].id),
+                                   headers={'X-XAPP-TOKEN': 'rudy-token'})
+        item = json.loads(res.get_data())
+        res = self.test_client.delete('api/v1/order_line_items/' + str(self.items[0].id),
+                                      headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 200)
+        deleted_item = json.loads(res.get_data())
+        self.assertDictEqual(item, deleted_item)
+        self.assertEqual(len(OrderLineItem.objects()), 3)
 
     def test_delete_a_non_existing_order_line_item(self):
-        pass
+        res = self.test_client.delete('api/v1/order_line_items/' + str(ObjectId()),
+                                      headers={'X-XAPP-TOKEN': 'rudy-token'})
+        self.assertEqual(res.status_code, 404)
+        self.assertIn("order line item not found", res.data)
 
 if __name__ == '__main__':
     unittest.main()
