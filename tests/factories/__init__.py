6fb9e519acee961b640c81a386b8d7bde2a47f0b
Chung-Yi Chi <cychi1210@gmail.com>
2015-03-03 19:00:03 -0700

    Add ClientAppFactory.

--- tests/factories/__init__.py
+++ tests/factories/__init__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
+from client_app_factory import *
 from user_factory import *
 from merchant_factory import *
 from order_factory import *
 from allpay_account_factory import *
 from order_payment_factory import *
 
-__all__ = (user_factory.__all__ +
+__all__ = (client_app_factory.__all__ +
+           user_factory.__all__ +
            merchant_factory.__all__ +
            order_factory.__all__ +
            allpay_account_factory.__all__ +
