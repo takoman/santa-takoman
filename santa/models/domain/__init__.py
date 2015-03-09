24721d7fab8186e19d00b6577939c245e0f39aae
Chung-Yi Chi <cychi1210@gmail.com>
2015-03-09 00:44:06 -0400

    Add Invoice and InvoiceLineItem models.

--- santa/models/domain/__init__.py
+++ santa/models/domain/__init__.py
@@ -6,6 +6,8 @@ from santa.models.domain.user import *
 from santa.models.domain.merchant import *
 from santa.models.domain.order import *
 from santa.models.domain.order_line_item import *
+from santa.models.domain.invoice import *
+from santa.models.domain.invoice_line_item import *
 from santa.models.domain.order_payment import *
 from santa.models.domain.product import *
 from santa.models.domain.payment_account import *
@@ -17,6 +19,8 @@ __all__ = (client_app.__all__ +
            merchant.__all__ +
            order.__all__ +
            order_line_item.__all__ +
+           invoice.__all__ +
+           invoice_line_item.__all__ +
            order_payment.__all__ +
            product.__all__ +
            payment_account.__all__ +
