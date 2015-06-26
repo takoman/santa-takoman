f5732392d7e5edf6f21f637a1f90c6d4cb77a2b8
Chung-Yi Chi <cychi1210@gmail.com>
2015-06-26 01:13:04 -0400

    Use update_with_validation helper in products endpoint.

--- santa/models/domain/product.py
+++ santa/models/domain/product.py
@@ -3,11 +3,12 @@
 from mongoengine import *
 from santa.models.domain.image import Image
 from santa.models.mixins.updated_at_mixin import UpdatedAtMixin
+from santa.models.mixins.updatable_mixin import UpdatableMixin
 import datetime
 
 __all__ = ('Product',)
 
-class Product(UpdatedAtMixin, Document):
+class Product(UpdatableMixin, UpdatedAtMixin, Document):
     title             = StringField(max_length=200, required=True)
     seller_product_id = StringField(max_length=40)  # Product ID from the seller to help identify the same product, e.g. ASIN on Amazon.
     brand             = StringField(max_length=200)
